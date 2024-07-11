9bf4a61deeb33fbb6ffd3e6242b6d4aee9117961
Rahad-Ullah <myemail.abc20@gmail.com>
2024-07-11 07:12:20 +0600

    Retriving all products has been implemented

--- src/app/modules/Product/product.controller.ts
+++ src/app/modules/Product/product.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from '../../utils/catchAsync'
 import { ProductServices } from './product.service'
 import sendResponse from '../../utils/sendResponse'
 import httpStatus from 'http-status'
+import AppError from '../../errors/AppError'
 
 // create new product
 const createProduct: RequestHandler = catchAsync(
@@ -18,6 +19,32 @@ const createProduct: RequestHandler = catchAsync(
   },
 )
 
+// get all products
+const getAllProducts: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {
+    const result = await ProductServices.getAllProductsFromDB()
+
+    // check if the result is empty
+    if (!result.length) {
+      sendResponse(res, {
+        success: false,
+        statusCode: httpStatus.NOT_FOUND,
+        message: 'No Data Found',
+        data: result,
+      })
+      return
+    }
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: 'Products retrieved successfully',
+      data: result,
+    })
+  },
+)
+
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
 }
--- src/app/modules/Product/product.service.ts
+++ src/app/modules/Product/product.service.ts
@@ -3,6 +3,7 @@ import { TProduct } from './product.interface'
 import { Product } from './product.model'
 import httpStatus from 'http-status'
 
+// create new product
 const createProductIntoDB = async (payload: TProduct) => {
   const { title } = payload
 
@@ -13,10 +14,17 @@ const createProductIntoDB = async (payload: TProduct) => {
   }
 
   const result = await Product.create(payload)
+  return result
+}
+
+// get all products
+const getAllProductsFromDB = async () => {
+  const result = await Product.find()
 
   return result
 }
 
 export const ProductServices = {
   createProductIntoDB,
+  getAllProductsFromDB,
 }
